4e88a2ca5319eccb62924b56295a8631e791fe8e
szbgreg <szbgreg@gmail.com>
2020-12-17 12:42:25 +0100

    Adding addExpense & removeExpense actions

--- src/redux.js
+++ src/redux.js
@@ -1,7 +1,33 @@
 import { createStore, combineReducers } from 'redux';
+import { v4 as uuidv4 } from 'uuid';
+
+const addExpense = ({
+  description = '',
+  note = '',
+  amount = 0,
+  createdAt = 0
+} = {}) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuidv4(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+const removeExpense = ({ id }) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
 
 const expensesReducer = (state = [], action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      return state.filter((e) => e.id !== action.id);
     default:
       return state;
   }
@@ -26,4 +52,11 @@ const store = createStore(
   combineReducers({ expenses: expensesReducer, filters: filtersReducer })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(
+  addExpense({ description: 'asdf', note: 'leiras', amount: 12, createdAt: 1 })
+);
+store.dispatch(removeExpense({ id: 1 }));
