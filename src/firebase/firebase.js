84c41f43094831799c6ee5c4afa1391e71df70d9
szbgreg <szbgreg@gmail.com>
2020-12-26 14:56:27 +0100

    Creating a seperate testing database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyAvODe2gVFxIaia5TqS0H8Iu1rWz5kucCU',
-  authDomain: 'expensify-41dc6.firebaseapp.com',
-  databaseURL:
-    'https://expensify-41dc6-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'expensify-41dc6',
-  storageBucket: 'expensify-41dc6.appspot.com',
-  messagingSenderId: '359937123849',
-  appId: '1:359937123849:web:c858b53403c5b34da4bad8'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,4 +1,6 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Dotenv from 'dotenv';
 
+Dotenv.config({ path: '.env.test' });
 Enzyme.configure({ adapter: new Adapter() });
