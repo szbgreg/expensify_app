e5b618b7cfd2cffa704a4b8a73f549defd924fcb
szbgreg <szbgreg@gmail.com>
2020-12-29 19:25:39 +0100

    Adding page-header styling component

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpensesSummary = (props) => (
-  <div>
-    <h1>
-      Viewing {props.expenseCount}{' '}
-      {props.expenseCount > 1 ? 'expenses' : 'expense'} totalling{' '}
-      {numeral(props.expensesTotal / 100).format('$0,0.00')}
-    </h1>
+  <div className="page-header">
+    <div className="content-container">
+      <h1 className="page-header__title">
+        Viewing <span>{props.expenseCount} </span>
+        {props.expenseCount > 1 ? 'expenses' : 'expense'} totalling{' '}
+        <span>{numeral(props.expensesTotal / 100).format('$0,0.00')}</span>
+      </h1>
+      <div className="page-header__actions">
+        <Link className="button" to="/create">
+          Add expense
+        </Link>
+      </div>
+    </div>
   </div>
 );
 
